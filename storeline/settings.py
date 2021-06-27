3c0d5d8c9a08e014ebc21b0764a7216504194d67
hakim_amarullah <hakimamarullah@gmail.com>
2021-06-27 09:21:01 +0700

    new branch commit

--- storeline/settings.py
+++ storeline/settings.py
@@ -141,3 +141,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
