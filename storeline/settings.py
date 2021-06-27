779ffd765487ff7f60db53217ce9a3f875ab7bbe
hakim_amarullah <hakimamarullah@gmail.com>
2021-06-27 13:28:56 +0700

    static_root problem

--- storeline/settings.py
+++ storeline/settings.py
@@ -145,4 +145,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS =[
     os.path.join(BASE_DIR,'static')
 ]
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
